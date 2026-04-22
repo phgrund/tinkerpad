d4c2f0585178d714d9dc44df146034df7db6c23a
phgrund <phrgrund@gmail.com>
2026-04-22 12:25:45 -0300

    add tinker run

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,85 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+let activeExecutionTerminal: vscode.Terminal | undefined;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "tinkerpad" is now active!');
+const RUN_COMMAND = 'tinkerpad.runCurrentFile';
+const RUN_VERBOSE_COMMAND = 'tinkerpad.runCurrentFileVerbose';
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('tinkerpad.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from TinkerPad!');
-	});
+export function activate(context: vscode.ExtensionContext): void {
+  const runNormal = vscode.commands.registerCommand(RUN_COMMAND, async () => {
+    await executeCurrentTinkerpadFile(false);
+  });
 
-	context.subscriptions.push(disposable);
+  const runVerbose = vscode.commands.registerCommand(RUN_VERBOSE_COMMAND, async () => {
+    await executeCurrentTinkerpadFile(true);
+  });
+
+  context.subscriptions.push(runNormal, runVerbose);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): void {
+  activeExecutionTerminal?.dispose();
+  activeExecutionTerminal = undefined;
+}
+
+async function executeCurrentTinkerpadFile(verbose: boolean): Promise<void> {
+  const editor = vscode.window.activeTextEditor;
+
+  if (!editor) {
+    vscode.window.showErrorMessage('No active editor found. Open a .php file inside .tinkerpad.');
+    return;
+  }
+
+  const document = editor.document;
+
+  if (!isRunnableTinkerpadFile(document)) {
+    vscode.window.showErrorMessage('The active file must be a .php file inside a .tinkerpad folder.');
+    return;
+  }
+
+  const workspaceFolder = vscode.workspace.getWorkspaceFolder(document.uri);
+
+  if (!workspaceFolder) {
+    vscode.window.showErrorMessage('Could not determine a workspace folder for the current file.');
+    return;
+  }
+
+  const rawContent = document.getText();
+  const contentWithoutPhpOpenTag = stripLeadingPhpOpenTag(rawContent);
+
+  if (!contentWithoutPhpOpenTag.trim()) {
+    vscode.window.showWarningMessage('The current .tinkerpad file is empty after removing the opening <?php tag.');
+    return;
+  }
+
+  if (activeExecutionTerminal) {
+    activeExecutionTerminal.dispose();
+    activeExecutionTerminal = undefined;
+  }
+
+  const terminalName = verbose ? 'Tinkerpad (verbose)' : 'Tinkerpad';
+  activeExecutionTerminal = vscode.window.createTerminal({
+    name: terminalName,
+    cwd: workspaceFolder.uri.fsPath
+  });
+
+  const delimiter = '__TINKERPAD_INPUT__';
+  const artisanCommand = verbose ? 'php artisan tinker -vvv' : 'php artisan tinker';
+  const script = `${artisanCommand} <<'${delimiter}'\n${contentWithoutPhpOpenTag}\n${delimiter}`;
+
+  activeExecutionTerminal.show(true);
+  activeExecutionTerminal.sendText(script, true);
+}
+
+function isRunnableTinkerpadFile(document: vscode.TextDocument): boolean {
+  if (document.languageId !== 'php' && !document.fileName.toLowerCase().endsWith('.php')) {
+    return false;
+  }
+
+  const normalizedPath = document.uri.fsPath.replace(/\\/g, '/');
+  return /\/\.tinkerpad\//.test(normalizedPath);
+}
+
+function stripLeadingPhpOpenTag(content: string): string {
+  return content.replace(/^\uFEFF?\s*<\?php\b\s*/, '');
+}
